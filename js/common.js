ecb7fe54105c3411e1574eeba193ffdc4e148c0f
Ayal <antonovayal97@gmail.com>
2025-03-14 00:57:51 +0700

    fix

--- js/common.js
+++ js/common.js
@@ -17,16 +17,16 @@ document.addEventListener("DOMContentLoaded",(event) => {
             //"/agoy/index.html",
             //"/agoy/about.html",
             //"/agoy/complex-objects.html",
-            "/index.html",
-            "/about.html",
-            "/complex-objects.html",
-            "/media.html",
-            "/news.html",
-            "/team.html",
-            "/gen-plan.html",
-            "/location-and-infrastructure.html",
-            "/partners.html",
-            "/location.html"
+            "./index.html",
+            "./about.html",
+            "./complex-objects.html",
+            "./media.html",
+            "./news.html",
+            "./team.html",
+            "./gen-plan.html",
+            "./location-and-infrastructure.html",
+            "./partners.html",
+            "./location.html"
         ].map(normalizePath);
 
         /*Главная - / ..
