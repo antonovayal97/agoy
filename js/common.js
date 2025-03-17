7fab8a541522bf69e1fa36d8ea4a0bc04e96c28a
Ayal <antonovayal97@gmail.com>
2025-03-18 05:12:30 +0700

    fix

--- js/common.js
+++ js/common.js
@@ -806,7 +806,7 @@ document.addEventListener("DOMContentLoaded",(event) => {
         // Промис `ymaps3.ready` будет зарезолвлен, когда загрузятся все компоненты основного модуля API
         await ymaps3.ready;
 
-        const {YMap, YMapDefaultSchemeLayer} = ymaps3;
+        const {YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer} = ymaps3;
 
         // Иницилиазируем карту
         const map = new YMap(
